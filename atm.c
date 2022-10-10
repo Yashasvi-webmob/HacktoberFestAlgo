9e99e9f34ced1b46bb32b1bfd409aa02d4291d16
Harpreet1201 <91593651+Harpreet1201@users.noreply.github.com>
2022-10-10 15:19:27 +0530

    Create atm.c

---
+++ atm.c
@@ -0,0 +1,146 @@
+/* C Program to Display the ATM Transaction
+
+This C Program performs ATM transaction. The types of ATM transaction are
+
+1) Balance checking
+2) Cash withdrawal
+3) Cash deposition.
+
+
+1. Firstly initialize the ATM pin and amount with some random number.
+2. Take the ATM pin as input.
+3. If the input pin is equal to the initialized pin, then do the further operations.
+4. Use switch statement to do the operations like Balance checking, Cash withdrawal, Cash deposition etc.
+5. Use while loop to terminate or restart the process. */
+
+    #include <stdio.h>
+
+    unsigned long amount=1000, deposit, withdraw;
+
+    int choice, pin, k;
+
+    char transaction ='y';
+
+    void main()
+
+    {
+
+    	while (pin != 1520)
+
+    	{
+
+    		printf("ENTER YOUR SECRET PIN NUMBER:");
+
+    		scanf("%d", &pin);
+
+    		if (pin != 1520)
+
+    		printf("PLEASE ENTER VALID PASSWORD\n");
+
+    	}
+
+    	do
+
+    	{
+
+    		printf("***Welcome to ATM Service***\n");
+
+    		printf("1. Check Balance\n");
+
+    		printf("2. Withdraw Cash\n");
+
+    		printf("3. Deposit Cash\n");
+
+    		printf("4. Quit\n");
+
+    		printf("************?*************?*\n\n");
+
+    		printf("Enter your choice: ");
+
+    		scanf("%d", &choice);
+
+    		switch (choice)
+
+    		{
+
+    		case 1:
+
+    			printf("\n YOUR BALANCE IN Rs : %lu ", amount);
+
+    			break;
+
+    		case 2:
+
+    			printf("\n ENTER THE AMOUNT TO WITHDRAW: ");
+
+    			scanf("%lu", &withdraw);
+
+    			if (withdraw % 100 != 0)
+
+    			{
+
+    				printf("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
+
+    			}
+
+    			else if (withdraw >(amount - 500))
+
+    			{
+
+    				printf("\n INSUFFICENT BALANCE");
+
+    			}
+
+    			else
+
+    			{
+
+    				amount = amount - withdraw;
+
+    				printf("\n\n PLEASE COLLECT CASH");
+
+    				printf("\n YOUR CURRENT BALANCE IS%lu", amount);
+
+    			}
+
+    			break;
+
+    		case 3:
+
+    			printf("\n ENTER THE AMOUNT TO DEPOSIT");
+
+    			scanf("%lu", &deposit);
+
+                            amount = amount + deposit;
+
+    			printf("YOUR BALANCE IS %lu", amount);
+
+    			break;
+
+    		case 4:
+
+    			printf("\n THANK U USING ATM");
+
+    			break;
+
+    		default:
+
+    			printf("\n INVALID CHOICE");
+
+    		}
+
+    		printf("\n\n\n DO U WISH TO HAVE ANOTHER TRANSCATION?(y/n): \n");
+
+    		fflush(stdin);
+
+    		scanf("%c", &transaction);
+
+    		if (transaction == 'n'|| transaction == 'N')
+
+                        k = 1;
+
+    	} while (!k);
+
+    	printf("\n\n THANKS FOR USING OUT ATM SERVICE");
+
+    }
